aa8b44baaf718f26aedd1d037f309f153b6711fe
Keerthy Suresh <78009245+keerthysuresh@users.noreply.github.com>
2023-06-12 15:37:13 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Employee
+{
+int e_no;
+String e_name;
+float e_salary;
+Employee(int id,String name,float sal)
+{
+e_no=id;
+e_name=name;
+e_salary=sal;
+}
+} 
+class main
+{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+Scanner s = new Scanner(System.in);
+int id,i,flag=0;
+String name;
+float sal;
+System.out.println("enter the no.of employees:");
+int n=sc.nextInt();
+Employee[] obj=new Employee[n];
+for(i=0;i<n;i++)
+{
+System.out.println("Enter the details of employee: " + (i+1));
+System.out.println("employee no: ");
+id=sc.nextInt();
+System.out.println("employee name: ");
+name=s.nextLine();
+System.out.println("employee salary: ");
+sal=sc.nextFloat();
+obj[i]=new Employee(id,name,sal);
+}
+System.out.println("enter the employee id to be searched: ");
+int temp=sc.nextInt();
+for(i=0;i<n;i++)
+{
+if(obj[i].e_no==temp)
+{
+System.out.println("Result found!");
+flag=1;
+break;
+}
+} 
+if(flag==1)
+System.out.println("Employee name: "+ obj[i].e_name);
+else
+System.out.println("not found" );
+}
+}
